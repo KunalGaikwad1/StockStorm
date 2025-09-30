0402a87dd84d6b2aec232828c9ac2f38227d50f2
KunalGaikwad1 <kunalgaikwad9594@gamil.com>
2025-09-30 21:26:45 +0530

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>CLick me</Button>
+    </div>
+  );
+};
+
+export default Home;
